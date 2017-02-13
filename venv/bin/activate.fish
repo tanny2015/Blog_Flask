93420542146882b8bda2f4ef9acbdaf1782341ae
tanny <tanorigin@gmail.com>
2017-02-13 17:47:21 +0800

    虚拟环境修正及helloWorld

--- venv/bin/activate.fish
+++ venv/bin/activate.fish
@@ -36,7 +36,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/tanny/Desktop/flask_m/venv"
+set -gx VIRTUAL_ENV "/Users/tanny/Desktop/Flask_Python/venv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
