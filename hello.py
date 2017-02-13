4046ee8afa839927c5c630658ecb171691ace7d4
tanny <tanorigin@gmail.com>
2017-02-13 18:04:41 +0800

    2-2 hello-tanny dynamic name

--- hello.py
+++ hello.py
@@ -1,3 +1,5 @@
+#coding=utf-8
+
 from flask import Flask
 app = Flask(__name__)
 
@@ -5,6 +7,15 @@ app = Flask(__name__)
 def index():
     return '<h1>Hello World!</h1>'
 
+
+# 添加了动态的一些东西
+
+@app.route('/user/<name>')
+def user(name):
+    return '<h1>Hello, %s!</h1>' %name
+
+
 if __name__ == '__main__':
     app.run(debug=True)
 
+# 测试URL :http://127.0.0.1:5000/user/tanny
