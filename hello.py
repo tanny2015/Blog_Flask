658653c1b18a49a3a50e518a533f0fcbbdd25d0f
tanny <tanorigin@gmail.com>
2017-03-04 19:53:48 +0800

    实例2-3 使用flask-script [2c]

--- hello.py
+++ hello.py
@@ -1,21 +1,26 @@
 #coding=utf-8
 
 from flask import Flask
+from flask_script import Manager
+
 app = Flask(__name__)
+
+manager = Manager(app)
+
 
 @app.route('/')
 def index():
     return '<h1>Hello World!</h1>'
 
 
-# 添加了动态的一些东西
-
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' %name
+    return '<h1>Hello, %s!</h1>' % name
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    manager.run()
 
+
+# 实例2-3 使用flask-script [2c]
 # 测试URL :http://127.0.0.1:5000/user/tanny
