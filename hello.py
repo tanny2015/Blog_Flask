5217e5a53fca26dbf503858771394d2c67181793
tanny <tanorigin@gmail.com>
2017-03-07 23:09:12 +0800

    3-5 使用Flask-Bootstrap的模板 [3b]

--- hello.py
+++ hello.py
@@ -2,11 +2,15 @@
 
 from flask import Flask, render_template
 from flask_script import Manager
+# 新增(注意：注释和#之间 是要有一个空格的！)
+
+from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
-
+# 新增
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def index():
@@ -22,7 +26,7 @@ if __name__ == '__main__':
     manager.run()
 
 
-# 实例3-3 jinja2 hello.py 渲染模板 [3a]
-# 注意这边一直报错是因为：你要把template那个文件夹去拷贝过来啊！！！
+# 实例3-5 使用Flask-Bootstrap的模板 [3b]
+
 # 测试URL1 :http://127.0.0.1:5000
 # 测试URL2 :http://127.0.0.1:5000/user/tanny
