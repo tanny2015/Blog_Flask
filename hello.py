6f2d95c7f590e689fb2e4a49a442524fafa1c368
tanny <tanorigin@gmail.com>
2017-03-07 21:20:02 +0800

    3-3 jinja2 hello.py 渲染模板 [3a]

--- hello.py
+++ hello.py
@@ -1,6 +1,6 @@
 #coding=utf-8
 
-from flask import Flask
+from flask import Flask, render_template
 from flask_script import Manager
 
 app = Flask(__name__)
@@ -10,17 +10,19 @@ manager = Manager(app)
 
 @app.route('/')
 def index():
-    return '<h1>Hello World!</h1>'
+    return render_template('index.html')
 
 
 @app.route('/user/<name>')
 def user(name):
-    return '<h1>Hello, %s!</h1>' % name
+    return render_template('user.html',name=name)
 
 
 if __name__ == '__main__':
     manager.run()
 
 
-# 实例2-3 使用flask-script [2c]
-# 测试URL :http://127.0.0.1:5000/user/tanny
+# 实例3-3 jinja2 hello.py 渲染模板 [3a]
+# 注意这边一直报错是因为：你要把template那个文件夹去拷贝过来啊！！！
+# 测试URL1 :http://127.0.0.1:5000
+# 测试URL2 :http://127.0.0.1:5000/user/tanny
