84b1c5c69580896eed1dbe2713ca556d2164fcb9
tanny <tanorigin@gmail.com>
2017-03-08 02:59:02 +0800

    3-9 使用模板继承机制简化页面模板 [3c]

--- hello.py
+++ hello.py
@@ -2,15 +2,21 @@
 
 from flask import Flask, render_template
 from flask_script import Manager
-# 新增(注意：注释和#之间 是要有一个空格的！)
-
+# (注意：注释和#之间 是要有一个空格的！)
 from flask_bootstrap import Bootstrap
 
 app = Flask(__name__)
 
 manager = Manager(app)
-# 新增
 bootstrap = Bootstrap(app)
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'),404
+
+@app.errorhandler(500)
+def internal_server_error(e):
+    return render_template('505.html'),500
 
 @app.route('/')
 def index():
@@ -26,7 +32,8 @@ if __name__ == '__main__':
     manager.run()
 
 
-# 实例3-5 使用Flask-Bootstrap的模板 [3b]
+# 实例3-9 使用模板继承机制简化页面模板 [3c]
 
-# 测试URL1 :http://127.0.0.1:5000
-# 测试URL2 :http://127.0.0.1:5000/user/tanny
+# 测试URL1 404  http://127.0.0.1:5000/123
+# 测试URL2 500  这个目前还不知道要怎么测试！
+
