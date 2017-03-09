b67e052b72f02fcdf05ae6983e863db2b453abfd
tanny <tanorigin@gmail.com>
2017-03-10 04:17:21 +0800

    4-1 Flask-WTF Web表单 [4a]

--- hello.py
+++ hello.py
@@ -1,45 +1,120 @@
 #coding=utf-8
-
-from datetime import datetime
 from flask import Flask, render_template
 from flask_script import Manager
 # (注意：注释和#之间 是要有一个空格的！)
 
 from flask_bootstrap import Bootstrap
 from flask_moment import Moment
+# from flask_wtf import Form 这个已经过期了
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'hard to guess string'
 
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 # 之前写成moment = Moment() 导致一直出错，说是moment未定义！真是浪费了不少时间啊！
 moment = Moment(app)
 
+class NameForm(FlaskForm):
+    name = StringField('What is your name?', validators=[DataRequired()])
+    submit = SubmitField('Submit')
+
+
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('404.html'),404
+
 
 @app.errorhandler(500)
 def internal_server_error(e):
     return render_template('500.html'),500
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('index.html', current_time=datetime.utcnow())
-
-
-@app.route('/user/<name>')
-def user(name):
-    return render_template('user.html', name=name)
-
+    name = None
+    # 这个是先新建一个输入表格，这个是自定义实现。具体实现在上边
+    form = NameForm()
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = ''
+    return render_template('index.html', form=form, name=name)
 
 if __name__ == '__main__':
     manager.run()
 
 
-# 实例3-13 加入一个datetime变量 时间显示 [3e]
+# 实例4-1 Flask-WTF Web表单 [4a]
 # 测试URL1 404  http://127.0.0.1:5000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
