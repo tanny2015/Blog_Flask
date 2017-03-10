6df0299fb429160e5785d7e2f7c51f184b80e1a0
tanny <tanorigin@gmail.com>
2017-03-10 18:23:30 +0800

    4-5 重定向和用户会话session [4b]

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 #coding=utf-8
-from flask import Flask, render_template
+from flask import Flask, render_template, session,  redirect, url_for
 from flask_script import Manager
 # (注意：注释和#之间 是要有一个空格的！)
 
@@ -36,19 +36,18 @@ def internal_server_error(e):
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    name = None
     # 这个是先新建一个输入表格，这个是自定义实现。具体实现在上边
     form = NameForm()
     if form.validate_on_submit():
-        name = form.name.data
-        form.name.data = ''
-    return render_template('index.html', form=form, name=name)
+        session['name'] = form.name.data
+        return redirect(url_for('index'))
+    return render_template('index.html', form=form, name=session.get('name'))
 
 if __name__ == '__main__':
     manager.run()
 
 
-# 实例4-1 Flask-WTF Web表单 [4a]
+# 实例4-5 重定向和用户会话session [4b]
 # 测试URL1 404  http://127.0.0.1:5000
 
 
