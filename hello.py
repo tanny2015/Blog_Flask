b398a82195722d4c8724cd390e5dde2e9aac1a49
tanny <tanorigin@gmail.com>
2017-04-03 20:52:18 +0800

    6-4  1.使用FLASK-Email 发送邮件  2.集成了pycharm的debug功能 [6a]

--- hello.py
+++ hello.py
@@ -1,7 +1,7 @@
  #coding=utf-8
 import os
 from flask import Flask, render_template, session,  redirect, url_for
-from flask_script import Manager,Shell
+from flask_script import Manager,Server,Shell
 # (注意：注释和#之间 是要有一个空格的！)
 
 from flask_bootstrap import Bootstrap
@@ -12,6 +12,7 @@ from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate, MigrateCommand
+from flask_mail import Mail,Message
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
@@ -20,12 +21,22 @@ app.config['SECRET_KEY'] = 'hard to guess string'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir,'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
 
+app.config['MAIL_SERVER'] = 'smtp.googlemail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = os.environ.get('MAIL_USERNAME')
+app.config['MAIL_PASSWORD'] = os.environ.get('MAIL_PASSWORD')
+app.config['FLASKY_MAIL_SUBJECT_PREFIX'] = '[Flasky]'
+app.config['FLASKY_MAIL_SENDER'] = 'Flasky Admin <flasky@example.com>'
+app.config['FLASKY_ADMIN'] = os.environ.get('FLASKY_ADMIN')
+
 manager = Manager(app)
 bootstrap = Bootstrap(app)
 # 之前写成moment = Moment() 导致一直出错，说是moment未定义！真是浪费了不少时间啊！
 moment = Moment(app)
 db = SQLAlchemy(app)
 migrate = Migrate(app,db)
+mail = Mail(app)
 
 class Role(db.Model):
      __tablename__ = 'roles'
@@ -46,12 +57,22 @@ class User(db.Model):
         return '<User %>' % self.name
 
 
+def send_mail(to, subject, template, **kwargs):
+    msg = Message(app.config['FLASKY_MAIL_SUBJECT_PREFIX'] + ' ' + subject,
+                  sender=app.config['FLASKY_MAIL_SENDER'],
+                  recipients=[to])
+    msg.body = render_template(template + '.txt',**kwargs)
+    msg.html = render_template(template + '.html', **kwargs)
+    mail.send(msg)
+
+
 class NameForm(FlaskForm):
     name = StringField('What is your name?', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
 def make_shell_context():
     return dict(app=app,db=db,User=User, Role=Role)
+manager.add_command("runserver", Server(use_debugger=True)) #方便debug
 manager.add_command("shell",Shell(make_context=make_shell_context))
 manager.add_command('db',MigrateCommand)
 
@@ -76,6 +97,9 @@ def index():
             user = User(username=form.name.data)
             db.session.add(user)
             session['known'] = False
+            if app.config['FLASKY_ADMIN']:
+                send_mail(app.config['FLASKY_ADMIN'],'New User',
+                          'mail/new_user',user=user)
         # 这个名字已经收录过，就不用再次收录
         else:
             session['known'] = True
@@ -90,7 +114,7 @@ if __name__ == '__main__':
 
 
 
-# 5.11.2 创建迁移脚本 数据库文件多出来一个version相关的表 [5d]
+# 6-4  1.使用FLASK-Email 发送邮件  2.集成了pycharm的debug功能 [6a]
 # 测试URL1 404  http://127.0.0.1:5000
 
 
