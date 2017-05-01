98e69dd82a24758e0ec3b0a9d1545d10cafd8278
tanny <tanorigin@gmail.com>
2017-05-01 17:02:16 +0800

    12.显示Follower关注者，修复之前Edit Admin的一个bug【12b】

--- config.py
+++ config.py
@@ -14,6 +14,7 @@ class Config:
     FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
     FLASKY_POSTS_PER_PAGE = 5
+    FLASKY_FOLLOWERS_PER_PAGE = 5
 
     @staticmethod
     def init_app(app):
